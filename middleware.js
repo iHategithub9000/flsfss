fc02c5a39565836a082e60374e47a8808ff89cb4
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2024-03-18 06:35:47 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,8 +1,13 @@
-var connectionCnt = 0;
+let connectionCnt = 0;
+const clc = require("cli-color");
+const error = clc.red.bold;
+const warn = clc.yellow.bold;
+const info = clc.blue;
+const debug = clc.magenta;
 module.exports = function(req, res, password, next) {
   if (require("./config.json")["log"]) {
     connectionCnt++;
-    console.log(require("./config.json")["server-texts"]["log.connection.first"] + connectionCnt + ": " + req.method + " " + req.url + require("./config.json")["server-texts"]["log.connection.second"] + req.get('User-Agent') + require("./config.json")["server-texts"]["log.connection.third"] + req.headers.origin)
+    console.log(info(require("./config.json")["server-texts"]["log.connection.first"] + connectionCnt + ": " + req.method + " " + req.url + require("./config.json")["server-texts"]["log.connection.second"] + req.get('User-Agent') + require("./config.json")["server-texts"]["log.connection.third"] + req.headers.origin))
   }
   if (require("./config.json")["restrict-origin"]) {
     if (req.headers.origin && (req.headers.origin === require("./config.json")["allowed-origin"])) {
@@ -37,7 +42,7 @@ module.exports = function(req, res, password, next) {
   for (const key in req.query) {
     if (req.query.hasOwnProperty(key) && req.query[key].length > require("./config.json")["max-query-param-length"]) {
       delete req.query[key];
-      if (require("./config.json")["log"]) console.log(require("./config.json")["server-texts"]["log.query.maxexceeded"])
+      if (require("./config.json")["log"]) console.log(warn(require("./config.json")["server-texts"]["log.query.maxexceeded"]))
     }
   }
 
